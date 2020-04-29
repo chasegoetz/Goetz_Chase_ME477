196a36b61e717556a23a2464ba8f286751ef46fa
chasegoetz <chase.goetz@stmartin.edu>
2020-04-29 15:14:52 -0700

    created msg directory, message_publisher, message_subscriber

---
+++ goetz_topics/src/message_publisher.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import rospy
+from goetz_topics.msg import Complex
+from random import random
+
+rospy.init_node('message_publisher')
+
+pub = rospy.Publisher(
+	'complex', 
+	Complex, 
+	queue_size=3
+)
+
+rate = rospy.Rate(2)
+
+while not rospy.is_shutdown():
+    msg = Complex()
+    msg.real = random()
+    msg.imaginary = random()
+
+    pub.publish(msg)
+    rate.sleep()
